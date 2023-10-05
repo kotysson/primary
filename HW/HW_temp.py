580b1d211bc042d2796aa80433f5957b6d864948
kotysson <soulestene@gmail.com>
2023-10-05 23:39:41 +0300

    Homework updated

--- HW/HW_temp.py
+++ HW/HW_temp.py
@@ -1,6 +1,4 @@
 temp_f = float(input("Введите температуру в градусах по Фаренгейту в формате xx.x: "))
 temp_c = (temp_f - 32) / 1.8
-print(temp_c)
-
-a = [20, 32.2, "fame"]
-b = a[5]
+temp_c = round(temp_c, 2)
+print("Температура в градусах по Цельсию: ", temp_c)
